756d1e22bbaa3c9a0d3f3731cd950899f1e606b5
SHANTANUSHINDE970 <ps1624814@gmail.com>
2026-01-18 21:20:39 +0530

    Update o.py

--- o.py
+++ o.py
@@ -2587,9 +2587,9 @@ with tab1:
     
     with col3:
         employee_email = st.text_input(
-            "📧 Email Address",
+            "📧 Email Address (Preferred_Address)",
             value=st.session_state.form_data_tab1['employee_email'],
-            placeholder="your.email@company.com",
+            placeholder="your_email",
             help="Your email address to receive notifications",
             key="employee_email_input"
         )
