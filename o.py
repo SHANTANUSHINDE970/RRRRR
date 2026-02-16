a3c5365516dc133f0fbd74c7e14f9095e8ba3227
SHANTANUSHINDE970 <ps1624814@gmail.com>
2026-02-16 20:52:08 +0530

    Update o.py

--- o.py
+++ o.py
@@ -1277,18 +1277,19 @@ def get_google_credentials():
         log_debug("Loading Google credentials from environment variables...")
         
         # Get credentials from environment variables
-        creds_dict = {
-            "type": os.getenv("GOOGLE_TYPE", "service_account"),
-            "project_id": os.getenv("GOOGLE_PROJECT_ID"),
-            "private_key_id": os.getenv("GOOGLE_PRIVATE_KEY_ID"),
-            "private_key": os.getenv("GOOGLE_PRIVATE_KEY"),
-            "client_email": os.getenv("GOOGLE_CLIENT_EMAIL"),
-            "client_id": os.getenv("GOOGLE_CLIENT_ID"),
-            "auth_uri": os.getenv("GOOGLE_AUTH_URI", "https://accounts.google.com/o/oauth2/auth"),
-            "token_uri": os.getenv("GOOGLE_TOKEN_URI", "https://oauth2.googleapis.com/token"),
-            "auth_provider_x509_cert_url": os.getenv("GOOGLE_AUTH_PROVIDER_X509_CERT_URL", "https://www.googleapis.com/oauth2/v1/certs"),
-            "client_x509_cert_url": os.getenv("GOOGLE_CLIENT_X509_CERT_URL")
-        }
+    creds_dict = {
+                "type": st.secrets[secrets_key]["type"],
+                "project_id": St. Secrets[secrets_key]["project_id"],
+                "private_key_id": st.secrets[secrets_key]["private_key_id"],
+                "private_key": st.secrets[secrets_key]["private_key"],
+                "client_email": st.secrets[secrets_key]["client_email"],
+                "client_id": st.secrets[secrets_key]["client_id"],
+                "auth_uri": st.secrets[secrets_key]["auth_uri"],
+                "token_uri": st.secrets[secrets_key]["token_uri"],
+                "auth_provider_x509_cert_url": st.secrets[secrets_key]["auth_provider_x509_cert_url"],
+                "client_x509_cert_url": st.secrets[secrets_key]["client_x509_cert_url"]
+            }
+
         
         # Check if required fields exist
         required_fields = ["type", "project_id", "private_key_id", "private_key", "client_email"]
